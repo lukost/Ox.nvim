6eb6cca57ceb73e17daad2c0821c855cea75d833
lukost <you@example.com>
2025-02-16 16:58:45 +0100

    Fix positioning and highlighting positions

--- lua/Ox/highlighter.lua
+++ lua/Ox/highlighter.lua
@@ -1,31 +1,17 @@
 local M = {}
+M.u_buf = require('Ox.bufutils')
 
 -- Function to update highlight based on cursor position
 M.update_highlight = function(bufnum)
 	local cursor = vim.api.nvim_win_get_cursor(0)
 	local row, col = cursor[1] - 1, cursor[2] -- Convert to 0-based index
-	local config = M.config[bufnum]
-	local group_size = config.group
-	local cols = config.cols
-	local addr_offset = config.addrlen + 2
-
-	local hex_start = addr_offset + 1
-	local hex_width = (cols*2)+(cols/group_size)-1
-	local hex_end = hex_start + hex_width
+	local ccol = M.u_buf.get_hex_preview_position(col, M.config[bufnum])
 	
-	local preview_start = hex_end + 1
-
-	if (col < hex_start) or (col > hex_end) then
-		vim.api.nvim_buf_clear_namespace(bufnum, M.h_ns, 0, -1)
-		return
-	end
-
-	local byte_index = (col - hex_start) - math.floor((col - hex_start) / (2 * group_size + 1))
-	local char_col = preview_start + math.floor(byte_index / 2)
-
 	-- Clear previous highlights and apply new one
 	vim.api.nvim_buf_clear_namespace(bufnum, M.h_ns, 0, -1)
-	vim.api.nvim_buf_add_highlight(bufnum, M.h_ns, "Cursor", row, char_col, char_col + 1)
+	if (ccol >= 0) then
+		vim.api.nvim_buf_add_highlight(bufnum, M.h_ns, "Cursor", row, ccol, ccol + 1)
+	end
 end
 
 -- Function to set up autocmd for cursor movement
